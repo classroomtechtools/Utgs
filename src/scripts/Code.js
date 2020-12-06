4a6e2d9a434552a800d1e949aa1d9f1fb73773be
Adam Morris <amorris@mistermorris.com>
2020-12-06 21:01:02 +0800

    more examples and @see tags

--- src/scripts/Code.js
+++ src/scripts/Code.js
@@ -1,6 +1,7 @@
 /**
  * Returns the module object, which has `describe`, `it`, and `assert` properties.
  * @returns {module:Utgs.UtgsObject}
+ * @see https://classroomtechtools.github.io/Utgs/global.html#module
  * @example
  * // init the variables
  * const {describe, it, assert} = utgs.module();
